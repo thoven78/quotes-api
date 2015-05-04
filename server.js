367f23c200725f83b1c1516ebb42e0b0ca03cb7f
Stevenson Michel <thoven87@gmail.com>
2015-05-04 16:50:43 -0400

    Added more Voltaire quotes

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ var quotes = [
   {
     author: 'Voltaire',
     text: 'With great power comes great responsability'
+  },
+  {
+    author: 'Voltaire',
+    text: 'God gave us the gift of life; it is up to us to give ourselves the gift of living well.'
+  },
+  {
+    author: 'Voltaire',
+    text: 'It is difficult to free fools from the chains they revere.'
   }
 ];
 
