2ac39309c6a94513df592e66742da1496a981f3e
Stevenson Michel <thoven87@gmail.com>
2015-05-05 09:46:54 -0400

    Added a quote model

---
+++ api/models/quote.js
@@ -0,0 +1,14 @@
+exports.quotes = [
+  {
+    author: 'Voltaire',
+    text: 'With great power comes great responsability'
+  },
+  {
+    author: 'Voltaire',
+    text: 'God gave us the gift of life; it is up to us to give ourselves the gift of living well.'
+  },
+  {
+    author: 'Voltaire',
+    text: 'It is difficult to free fools from the chains they revere.'
+  }
+];
