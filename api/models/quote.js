1c0ac5412aa5a3130fd2e39d82a7e1ffebe2ba75
Stevenson Michel <thoven87@gmail.com>
2015-05-06 17:07:16 -0400

    Changed quote schema added a random method to get a random quote

--- api/models/quote.js
+++ api/models/quote.js
@@ -5,14 +5,34 @@ var mongoose = require('mongoose');
 var Schema  = mongoose.Schema;
 
 var QuoteSchema = new Schema({
-  author: String,
-  text: String,
+  author: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
   _creator: {
     type: Schema.ObjectId,
     ref: 'User'
   },
-  createdAt: Date,
-  updatedAt: Date
+  createdAt: {
+    type: Date,
+    required: true
+  },
+  updatedAt: {
+    type: Date,
+    required: true
+  }
 });
+
+QuoteSchema.statics.random = function random(cb) {
+  this.count(function count(err, count) {
+    if (err) cb(err);
+    var rand = Math.floor(Math.random() * count);
+    this.findOne().skip(rand).exec(cb);
+  }.bind(this));
+};
 
 exports = mongoose.model('Quote', QuoteSchema);
