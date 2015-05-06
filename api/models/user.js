4c69fbe3a40298a9a7cdb1a88ce4696dd1d05369
Stevenson Michel <thoven87@gmail.com>
2015-05-06 17:07:46 -0400

    Changed the user schema and add validation

--- api/models/user.js
+++ api/models/user.js
@@ -3,11 +3,35 @@
 var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  createdAt: Date,
-  updatedAt: Date,
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true
+  },
+  salt: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    required: true
+  },
+  updatedAt: {
+    type: Date,
+    required: true
+  }
 });
 
 exports = mongoose.model('User', UserSchema);
