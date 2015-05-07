689656d2ca11e8b2fb5c0af70cebbca75eb55d85
Stevenson Michel <thoven87@gmail.com>
2015-05-07 15:54:55 -0400

    User auth cookie

--- api/components/auth.js
+++ api/components/auth.js
@@ -1,15 +1,23 @@
 'use strict';
 
+var Bell = require('bell');
+var AuthCookie = require('hapi-auth-cookie');
+
 module.exports = function isAuthenticated(server, config) {
   // OAuth
-  server.register(require('bell'), function bell(err) {
+  server.register([Bell, AuthCookie], function bell(err) {
+
+    if (err) {
+      console.error(err);
+      return process.exit(1);
+    }
 
     // Declare Auth strategy using the bell scheme
     // with the name of the provider, cookie encryption password
     // and the OAuth client credentials
     server.auth.strategy('github', 'bell', {
       provider: 'github',
-      password: 'cookie_encryption_password',
+      password: 'github-encryption-password', //Password used for encryption
       clientId: config.clientID,
       clientSecret: config.clientSecret,
       isSecure: false, // Terrible but for now we are using http, please change to true in production
@@ -18,7 +26,15 @@ module.exports = function isAuthenticated(server, config) {
       }
     });
 
-    // TODO add more strategies
+    // Declare Auth cookie strategy
+    server.auth.strategy('session', 'cookie', {
+      password: 'cookie-encryption-password', //Password used for encryption
+      cookie: 'sid-quote', // Name of cookie to set
+      isSecure: false
+    });
+
+    server.auth.default('session');
+
   });
 
 };
